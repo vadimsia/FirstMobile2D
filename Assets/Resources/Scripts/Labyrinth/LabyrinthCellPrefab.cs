4a44d61fcada8b08eae5b9f9c6fd21ab896919f0
Rimuru2202 <adik.budaev@mail.ru>
2025-03-13 17:59:24 +0300

    Сделал задачу "Добавить точку старта и финиша лабиринта"

--- Assets/Resources/Scripts/Labyrinth/LabyrinthCellPrefab.cs
+++ Assets/Resources/Scripts/Labyrinth/LabyrinthCellPrefab.cs
@@ -1,5 +1,6 @@
-using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 namespace Resources.Scripts.Labyrinth
 {
@@ -9,21 +10,66 @@ namespace Resources.Scripts.Labyrinth
         [SerializeField] private GameObject rightBorder;
         [SerializeField] private GameObject bottomBorder;
         [SerializeField] private GameObject leftBorder;
-        [SerializeField] private TextMeshProUGUI arrayValue;
-        
+        [SerializeField] private Text arrayValueText; // Используем Legacy Text
+
+        // Флаг для определения, что эта клетка является финишной
+        private bool isFinishCell;
+
+        private void Awake()
+        {
+            // Проверяем наличие BoxCollider2D на родительском объекте и добавляем его, если отсутствует
+            BoxCollider2D collider = GetComponent<BoxCollider2D>();
+            if (collider == null)
+            {
+                collider = gameObject.AddComponent<BoxCollider2D>();
+            }
+            // Устанавливаем флаг Is Trigger, чтобы обрабатывать столкновения через OnTriggerEnter2D
+            collider.isTrigger = true;
+        }
+
         public void Init(LabyrinthCell cell)
         {
             if (!cell.topBorder)
                 Destroy(topBorder);
-
             if (!cell.rightBorder)
                 Destroy(rightBorder);
-
             if (!cell.bottomBorder)
                 Destroy(bottomBorder);
-
             if (!cell.leftBorder)
                 Destroy(leftBorder);
+
+            // Настраиваем отображение текста и запоминаем, если клетка финишная
+            if (cell.isStart)
+            {
+                arrayValueText.text = "S";
+                arrayValueText.color = Color.green;
+            }
+            else if (cell.isFinish)
+            {
+                arrayValueText.text = "F";
+                arrayValueText.color = Color.red;
+                isFinishCell = true;
+            }
+            else if (cell.isSolutionPath)
+            {
+                arrayValueText.text = cell.arrayValue.ToString();
+                arrayValueText.color = Color.yellow;
+            }
+            else
+            {
+                arrayValueText.text = cell.arrayValue.ToString();
+                arrayValueText.color = Color.white;
+            }
+        }
+
+        // Обработка столкновения с игроком
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            // Если клетка финишная и столкновение произошло с объектом, у которого тег "Player"
+            if (isFinishCell && other.CompareTag("Player"))
+            {
+                SceneManager.LoadScene("FirstPartScene");
+            }
         }
     }
 }
